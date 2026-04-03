fa52006a8709c19066d9e8b87418380eb6c186e7
Guillaume Qu <guillaume.qu.1@etu.sorbonne-universite.fr>
2026-04-03 15:57:21 +0200

    debut q4

--- main.py
+++ main.py
@@ -8,5 +8,12 @@ exemple.createFichierLP(maListe[0][0],int(maListe[1][0])) #Methode int(): transf
 '''
 Etu=u.MatEtu("PrefEtu.txt") # Execution de la methode lectureFichier du fichier exemple.
 print(Etu)
-Spe=u.MatSpe("PrefSpe.txt") # Execution de la methode lectureFichier du fichier exemple.
+Spe, Cap=u.MatSpe("PrefSpe.txt") # Execution de la methode lectureFichier du fichier exemple.
 print(Spe)
+
+print(Cap)
+gs_etudiant = u.GS_Etudiant(Etu, Spe, Cap)
+gs_parcours = u.GS_Parcours(Etu, Spe, Cap)
+
+gs_etudiant.gs_etu()
+print(gs_etudiant.get_mariage())
--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def MatSpe(s): # Definition d'une fonction, avec un parametre (s). Ne pas oublie
     contenu.remove(contenu[0])
     capacite = contenu[0]
     contenu.remove(capacite)
-    capacite.split()
+    capacite = capacite.split()
     capacite.remove(capacite[0])
     for i in range(len(contenu)) :
         contenu[i] = contenu[i].split()  
@@ -26,15 +26,17 @@ def MatSpe(s): # Definition d'une fonction, avec un parametre (s). Ne pas oublie
         contenu[i].remove(contenu[i][0])
     return contenu, capacite
 
-class GS:
+class GS_Etudiant:
 
     def __init__(self, etu, spe, cap):
         self.etu = etu
         self.nb_etu = len(etu)
         self.spe = spe
+        self.nb_spe = len(spe)
         self.cap_max = cap
-        self.cap_actu = [[] for _ in range(len(spe))]
+        self.cap_actu = [[] for _ in range(self.nb_spe)]
         self.mariage = [-1] * self.nb_etu
+        self.proposition = [[] for _ in range(self.nb_etu)]
 
     def trouve_etu(self):
         for i in range(self.nb_etu):
@@ -43,15 +45,74 @@ class GS:
         return -1
 
     def trouve_spe(self, etu_ind):
-        for pref in self.etu[etu_ind]:
-            if len(self.cap_actu[pref]) == self.cap_max[pref]:
-                continue
-            self.cap_actu[pref].append(etu_ind)
-            self.mariage[etu_ind] = pref
-            return
+        propose = len(self.proposition[etu_ind])
+        if propose < self.nb_spe :
+            self.proposition[etu_ind].append(self.etu[etu_ind][propose])
+            return self.etu[etu_ind][propose]
+        return -1
 
     def pos_etu(self, etu_ind, spe_ind):
         return self.spe[spe_ind].index(etu_ind)
+
+    def least_pref(self, spe_ind):
+        pos_least = -1
+        pos_etu = -1
+        rep
+        for etu in self.cap_actu[spe_ind] :
+            pos_etu = self.pos_etu(etu, spe_ind)
+            if pos_least < pos_etu :
+                pos_least = pos_etu
+                rep = etu
+        return rep, pos_least
+
+    def remplacer(self, etu1, etu2, spe_ind) :
+        self.cap_actu[spe_ind].remove(etu1)
+        self.cap_actu[spe_ind].append[etu2]
+        self.mariage[etu1] = -1
+        self.mariage[etu2] = spe_ind
+
+    def gs_etu(self) :
+        etu = self.trouve_etu()
+        while etu != -1 :
+            spe = self.trouve_spe(etu)
+            if len(self.cap_actu[spe]) < self.cap_max[spe] :
+                self.mariage[etu] = spe
+                self.cap_actu[spe].append(etu)
+            else :
+                lp, lp_pos = self.least_pref(spe)
+                if self.pos_etu(etu, spe) < lp_pos :
+                    self.remplacer(lp, etu, spe)
+            etu = self.trouve_etu()    
+
+    def get_mariage(self) :
+        return self.mariage
+
+class GS_Parcours:
+
+    def __init__(self, etu, spe, cap):
+        self.etu = etu
+        self.nb_etu = len(etu)
+        self.spe = spe
+        self.nb_spe = len(spe)
+        self.cap_max = cap
+        self.cap_actu = [[] for _ in range(self.nb_spe)]
+        self.mariage = [-1] * self.nb_etu
+        self.proposition = [[] for _ in range(self.nb_spe)]
+
+    def trouve_etu(self, spe_ind):
+        propose = len(self.proposition[spe])
+        if propose < self.nb_etu :
+            self.proposition[spe_ind].append(self.spe[spe_ind][propose])
+            return self.spe[spe_ind][propose]
+        return -1
+
+    def trouve_spe(self):
+        for i, p in enumerate(self.proposition) :
+            if len(p) < self.cap_max[i] :
+                return i
+
+    def pos_spe(self, etu_ind, spe_ind):
+        return self.etu[etu_ind].index(spe_ind)
 
     def least_pref(self, spe_ind):
         pos_least = -1
@@ -62,12 +123,26 @@ class GS:
                 pos_least = pos_etu
         return pos_least
 
-    def remplacer(etu1, etu2, spe_ind) :
-        self.cap_actu[spe_ind].remove(etu1)
-        self.cap_actu[spe_ind].append[etu2]
-        self.mariage[etu1] = -1
-        self.mariage[etu2] = spe_ind
+    def remplacer(self, spe1, spe2, etu_ind) :
+        self.cap_actu[spe1].remove(etu_ind)
+        self.cap_actu[spe2].append[etu_ind]
+        self.mariage[etu_ind] = spe2
 
+    def gs_spe(self) :
+        spe = self.trouve_spe()
+        while spe != -1 :
+            etu = self.trouve_etu(spe)
+            if self.mariage[etu] == -1 :
+                self.mariage[etu] = spe
+                self.cap_actu[spe].append(etu)
+            else :
+                lp = self.mariage[etu]
+                if self.pos_spe(etu) < self.pos_spe(lp) :
+                    self.remplacer(lp, etu, spe)
+            spe = self.trouve_spe()
+
+    def get_mariage(self) :
+        return self.mariage
 
 
 def createFichierLP(nomFichier,nombreVariables):
